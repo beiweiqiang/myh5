b2004d305190c6cee930754dd83dbacbf058b8c2
beiweiqiang <beiweiqiang@qq.com>
2017-05-17 21:41:33 +0800

    添加config

---
+++ config/wechat.cfg.js
@@ -0,0 +1,18 @@
+// module.exports = {
+//   grant_type: 'client_credential',
+//   appid: 'wxac6603ca2683be67',
+//   secret: '04cff79bdfb6c657fe315f6bf7ef07b6',
+//   accessTokenUrl: 'https://api.weixin.qq.com/cgi-bin/token',
+//   ticketUrl: 'https://api.weixin.qq.com/cgi-bin/ticket/getticket',
+//   cache_duration: 1000 * 2, //根据规定缓存时长为2小时
+// };
+
+module.exports = {
+  grant_type: 'client_credential',
+  appid: 'wx24645cccbc603587',
+  secret: '392d780a827f6d1f576063925c0186eb',
+  noncestr: 'Wm3WZYTPz0wzccnW',
+  accessTokenUrl: 'https://api.weixin.qq.com/cgi-bin/token',
+  ticketUrl: 'https://api.weixin.qq.com/cgi-bin/ticket/getticket',
+  cache_duration: 1000 * 2, //缓存时长为2小时
+};
